9b96a8f4dadf84c0672d18d564514aea8db0d251
AndreyTlustenko <andreyt2018@ukr.net>
2022-10-04 20:55:23 +0300

    1

--- js/task-08.js
+++ js/task-08.js
@@ -13,14 +13,4 @@ docForm.reset();
 
 });
 
-// const form = document.querySelector(".login-form");
-// form.addEventListener("submit", (event) => { 
-//     event.preventDefault(); 
-//     const {elements: { email, password }} = event.currentTarget; 
-//     if (email.value === "" || password.value === "") { 
-//       alert("Всі поля повинні бути заповнені!");
-//     } else {
-//     console.log({ "email": email.value, "password": password.value });
-//       form.reset();
-//     }
-//   });
+// 
