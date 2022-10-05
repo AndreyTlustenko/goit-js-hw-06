fd2dd9c1e9c2a64ed197aa0a6944514149aedd56
AndreyTlustenko <andreyt2018@ukr.net>
2022-10-05 18:20:06 +0300

    1

--- js/task-05.js
+++ js/task-05.js
@@ -3,6 +3,7 @@ const textInputRef = document.querySelector("#name-input");
 console.log(textInputRef)
 const outputRef = document.querySelector("#name-output");
 console.log(outputRef)
+
 textInputRef.addEventListener("input", (event) => {
     outputRef.textContent = event.currentTarget.value;
     if (event.currentTarget.value === "")  {
