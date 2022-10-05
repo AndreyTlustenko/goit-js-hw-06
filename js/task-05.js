814bfe4c552e91fcb867111a4a68fdc9fb7ac749
AndreyTlustenko <andreyt2018@ukr.net>
2022-10-05 18:09:25 +0300

    1

--- js/task-05.js
+++ js/task-05.js
@@ -4,15 +4,19 @@ console.log(textInputRef)
 const outputRef = document.querySelector("#name-output");
 console.log(outputRef)
 textInputRef.addEventListener("input", (event) => {
+    outputRef.textContent = event.currentTarget.value;
+    if (event.currentTarget.value === "")  {
+    outputRef.textContent = "Anonymous";}
+});    
    
-    const inputValue = event.target.value;
-    outputRef.textContent = `${inputValue}` ;
-});
-input.addEventListener("input",()=> {
-    if (input.value)  {
-      output.textContent = input.value;
-    } else {
-      output.textContent = "Anonymous";
-    }
-  });
+    // const inputValue = event.target.value;
+    // outputRef.textContent = `${inputValue}` ;
+
+// input.addEventListener("input",()=> {
+//     if (input.value)  {
+//       output.textContent = input.value;
+//     } else {
+//       output.textContent = "Anonymous";
+//     }
+//   });
 
